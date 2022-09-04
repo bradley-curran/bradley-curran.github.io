fa26cef6d2d82c6d2b54db158ef1f8cba113dbc8
Brad <brad@bradleycurran.com>
2022-09-04 16:04:11 -0700

    Replace bssg build tool with build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e # exit when any command fails
+set -v # echo commands
+
+# start from scratch each time
+rm -rf docs
+mkdir docs
+
+# config
+cp CNAME docs/CNAME
+
+# style
+bssg lessc style.less > docs/style.css
+
+# top-level pages
+bssg pugc pug/index.pug data.json > docs/index.html
+bssg pugc pug/rss.pug data.json > docs/rss.xml
+
+# blog
+mkdir docs/blog
+bssg markdownc blog/bssg.md pug/blog.pug > docs/blog/bssg.html
